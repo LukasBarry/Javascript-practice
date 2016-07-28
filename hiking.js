4bcf154741a05bbef459a25a638562803b17365c
Lukas <Lukasbarry@yahoo.com>
2016-07-28 15:51:22 -0400

    javascript work

---
+++ hiking.js
@@ -0,0 +1,9 @@
+var todaysTemperature = 42;
+
+if (todaysTemperature > 50 ) {
+   console.log("I'm going hiking!");
+} else if (todaysTemperature < 32 ) {
+  console.log("It is freezing!");
+} else {
+    console.log("meh");
+    }
---
+++ lesson01.js
@@ -0,0 +1,27 @@
+// var x = true;
+// if(x) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// var cups = prompt("How many cups of coffee have you had?")
+//
+// if (Number(cups) < 3) {
+//   console.log("Yes, I will take another cup of coffee")
+// } else {
+//   console.log("I think I'm ok for now")
+// };
+
+// var temp = 90;
+// var precip = false;
+// var indoors = true;
+//
+// console.log("The temperaure is " + temp + " degrees");
+// if ((temp > 80 && precip === false) || indoors === true) {
+//   console.log("Time to swim!")
+// };
+
+// for (var i = 99; i >= 1; i --) {
+//     console.log(i + " Bottles of Beer on the wall, " + i + " Bottles of Beer.");
+// }
